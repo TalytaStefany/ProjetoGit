1831dd9507eefbfeebe35e284d32bef5906023ab
talyta <talytastefany@gmail.com>
2019-10-03 22:54:19 -0300

    auls 03/10

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-var num1 =7;
-var num2 =5;
+const TelegramBot = require('node-telegram-bot-api');
 
-var soma= num1+num2;
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '938125828:AAF7dT1SDlrnC60MIxqVe9gov5kVTjMzkGU';
 
-console.log("Exemplo Node");
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
 
-if(soma > 10){
-	console.log("Parabens!");
-}else{
-	console.log("Menor que 10");
-}
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
+
+// Listen for any kind of message. There are different kinds of
+// messages.
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  //bot.sendMessage(chatId, 'Received your message');
+
+  var Hi ="hi";
+  if(msg.text.toString().toLowerCase().indexOf(Hi)===0){
+    bot.sendMessage(msg.chat.id, "Hello dear "+msg.chat.first_name)
+  }
+
+  var location ="location";
+  if(msg.text.toString().toLowerCase().indexOf(location)===0){
+    bot.sendLocation(msg.chat.id,1212,12121)
+  }
+});
